35a957de7817d20ef35091a44cdaeda455bbd9d0
andrii <andriikliui@gmail.com>
2025-09-11 00:08:44 +0200

    fix(decoders): add Key-based matching for Metaplex account decoding

--- decoders/mpl-token-metadata-decoder/src/accounts/mod.rs
+++ decoders/mpl-token-metadata-decoder/src/accounts/mod.rs
@@ -1,6 +1,7 @@
+use solana_pubkey::{pubkey, Pubkey};
 use carbon_core::account::AccountDecoder;
 use carbon_core::deserialize::CarbonDeserialize;
-
+use crate::types::Key;
 use super::TokenMetadataDecoder;
 pub mod collection_authority_record;
 pub mod edition;
@@ -40,172 +41,84 @@ impl AccountDecoder<'_> for TokenMetadataDecoder {
         &self,
         account: &solana_account::Account,
     ) -> Option<carbon_core::account::DecodedAccount<Self::AccountType>> {
-        if let Some(decoded_account) =
-            collection_authority_record::CollectionAuthorityRecord::deserialize(
-                account.data.as_slice(),
-            )
-        {
-            return Some(carbon_core::account::DecodedAccount {
-                lamports: account.lamports,
-                data: TokenMetadataAccount::CollectionAuthorityRecord(decoded_account),
-                owner: account.owner,
-                executable: account.executable,
-                rent_epoch: account.rent_epoch,
-            });
+        // Guard
+        let mpl_token_metadata_id: Pubkey = pubkey!("metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s");
+        if account.owner != mpl_token_metadata_id {
+            return None;
         }
 
-        if let Some(decoded_account) =
-            metadata_delegate_record::MetadataDelegateRecord::deserialize(account.data.as_slice())
-        {
-            return Some(carbon_core::account::DecodedAccount {
-                lamports: account.lamports,
-                data: TokenMetadataAccount::MetadataDelegateRecord(decoded_account),
-                owner: account.owner,
-                executable: account.executable,
-                rent_epoch: account.rent_epoch,
-            });
-        }
+        let tag = match account.data.first().copied() {
+            Some(b) => b,
+            None => return None,
+        };
 
-        if let Some(decoded_account) =
-            holder_delegate_record::HolderDelegateRecord::deserialize(account.data.as_slice())
-        {
-            return Some(carbon_core::account::DecodedAccount {
-                lamports: account.lamports,
-                data: TokenMetadataAccount::HolderDelegateRecord(decoded_account),
-                owner: account.owner,
-                executable: account.executable,
-                rent_epoch: account.rent_epoch,
-            });
-        }
+        let data_enum = match tag {
+            x if x == Key::MetadataV1 as u8 => {
+                metadata::Metadata::deserialize(&mut account.data.as_slice())
+                    .map(TokenMetadataAccount::Metadata)
+            }
+            x if x == Key::CollectionAuthorityRecord as u8 => {
+                collection_authority_record::CollectionAuthorityRecord::deserialize(&mut account.data.as_slice())
+                    .map(TokenMetadataAccount::CollectionAuthorityRecord)
+            }
+            x if x == Key::MetadataDelegate as u8 => {
+                metadata_delegate_record::MetadataDelegateRecord::deserialize(&mut account.data.as_slice())
+                    .map(TokenMetadataAccount::MetadataDelegateRecord)
+            }
+            x if x == Key::HolderDelegate as u8 => {
+                holder_delegate_record::HolderDelegateRecord::deserialize(&mut account.data.as_slice())
+                    .map(TokenMetadataAccount::HolderDelegateRecord)
+            }
+            x if x == Key::EditionV1 as u8 => {
+                edition::Edition::deserialize(&mut account.data.as_slice())
+                    .map(TokenMetadataAccount::Edition)
+            }
+            x if x == Key::EditionMarker as u8 => {
+                edition_marker::EditionMarker::deserialize(&mut account.data.as_slice())
+                    .map(TokenMetadataAccount::EditionMarker)
+            }
+            x if x == Key::EditionMarkerV2 as u8 => {
+                edition_marker_v2::EditionMarkerV2::deserialize(&mut account.data.as_slice())
+                    .map(TokenMetadataAccount::EditionMarkerV2)
+            }
+            x if x == Key::TokenOwnedEscrow as u8 => {
+                token_owned_escrow::TokenOwnedEscrow::deserialize(&mut account.data.as_slice())
+                    .map(TokenMetadataAccount::TokenOwnedEscrow)
+            }
+            x if x == Key::MasterEditionV2 as u8 => {
+                master_edition_v2::MasterEditionV2::deserialize(&mut account.data.as_slice())
+                    .map(TokenMetadataAccount::MasterEditionV2)
+            }
+            x if x == Key::MasterEditionV1 as u8 => {
+                master_edition_v1::MasterEditionV1::deserialize(&mut account.data.as_slice())
+                    .map(TokenMetadataAccount::MasterEditionV1)
+            }
+            x if x == Key::TokenRecord as u8 => {
+                token_record::TokenRecord::deserialize(&mut account.data.as_slice())
+                    .map(TokenMetadataAccount::TokenRecord)
+            }
+            x if x == Key::ReservationListV2 as u8 => {
+                reservation_list_v2::ReservationListV2::deserialize(&mut account.data.as_slice())
+                    .map(TokenMetadataAccount::ReservationListV2)
+            }
+            x if x == Key::ReservationListV1 as u8 => {
+                reservation_list_v1::ReservationListV1::deserialize(&mut account.data.as_slice())
+                    .map(TokenMetadataAccount::ReservationListV1)
+            }
+            x if x == Key::UseAuthorityRecord as u8 => {
+                use_authority_record::UseAuthorityRecord::deserialize(&mut account.data.as_slice())
+                    .map(TokenMetadataAccount::UseAuthorityRecord)
+            }
+            _ => None,
+        }?;
 
-        if let Some(decoded_account) = edition::Edition::deserialize(account.data.as_slice()) {
-            return Some(carbon_core::account::DecodedAccount {
-                lamports: account.lamports,
-                data: TokenMetadataAccount::Edition(decoded_account),
-                owner: account.owner,
-                executable: account.executable,
-                rent_epoch: account.rent_epoch,
-            });
-        }
-
-        if let Some(decoded_account) =
-            edition_marker::EditionMarker::deserialize(account.data.as_slice())
-        {
-            return Some(carbon_core::account::DecodedAccount {
-                lamports: account.lamports,
-                data: TokenMetadataAccount::EditionMarker(decoded_account),
-                owner: account.owner,
-                executable: account.executable,
-                rent_epoch: account.rent_epoch,
-            });
-        }
-
-        if let Some(decoded_account) =
-            edition_marker_v2::EditionMarkerV2::deserialize(account.data.as_slice())
-        {
-            return Some(carbon_core::account::DecodedAccount {
-                lamports: account.lamports,
-                data: TokenMetadataAccount::EditionMarkerV2(decoded_account),
-                owner: account.owner,
-                executable: account.executable,
-                rent_epoch: account.rent_epoch,
-            });
-        }
-
-        if let Some(decoded_account) =
-            token_owned_escrow::TokenOwnedEscrow::deserialize(account.data.as_slice())
-        {
-            return Some(carbon_core::account::DecodedAccount {
-                lamports: account.lamports,
-                data: TokenMetadataAccount::TokenOwnedEscrow(decoded_account),
-                owner: account.owner,
-                executable: account.executable,
-                rent_epoch: account.rent_epoch,
-            });
-        }
-
-        if let Some(decoded_account) =
-            master_edition_v2::MasterEditionV2::deserialize(account.data.as_slice())
-        {
-            return Some(carbon_core::account::DecodedAccount {
-                lamports: account.lamports,
-                data: TokenMetadataAccount::MasterEditionV2(decoded_account),
-                owner: account.owner,
-                executable: account.executable,
-                rent_epoch: account.rent_epoch,
-            });
-        }
-
-        if let Some(decoded_account) =
-            master_edition_v1::MasterEditionV1::deserialize(account.data.as_slice())
-        {
-            return Some(carbon_core::account::DecodedAccount {
-                lamports: account.lamports,
-                data: TokenMetadataAccount::MasterEditionV1(decoded_account),
-                owner: account.owner,
-                executable: account.executable,
-                rent_epoch: account.rent_epoch,
-            });
-        }
-
-        if let Some(decoded_account) = metadata::Metadata::deserialize(account.data.as_slice()) {
-            return Some(carbon_core::account::DecodedAccount {
-                lamports: account.lamports,
-                data: TokenMetadataAccount::Metadata(decoded_account),
-                owner: account.owner,
-                executable: account.executable,
-                rent_epoch: account.rent_epoch,
-            });
-        }
-
-        if let Some(decoded_account) =
-            token_record::TokenRecord::deserialize(account.data.as_slice())
-        {
-            return Some(carbon_core::account::DecodedAccount {
-                lamports: account.lamports,
-                data: TokenMetadataAccount::TokenRecord(decoded_account),
-                owner: account.owner,
-                executable: account.executable,
-                rent_epoch: account.rent_epoch,
-            });
-        }
-
-        if let Some(decoded_account) =
-            reservation_list_v2::ReservationListV2::deserialize(account.data.as_slice())
-        {
-            return Some(carbon_core::account::DecodedAccount {
-                lamports: account.lamports,
-                data: TokenMetadataAccount::ReservationListV2(decoded_account),
-                owner: account.owner,
-                executable: account.executable,
-                rent_epoch: account.rent_epoch,
-            });
-        }
-
-        if let Some(decoded_account) =
-            reservation_list_v1::ReservationListV1::deserialize(account.data.as_slice())
-        {
-            return Some(carbon_core::account::DecodedAccount {
-                lamports: account.lamports,
-                data: TokenMetadataAccount::ReservationListV1(decoded_account),
-                owner: account.owner,
-                executable: account.executable,
-                rent_epoch: account.rent_epoch,
-            });
-        }
-
-        if let Some(decoded_account) =
-            use_authority_record::UseAuthorityRecord::deserialize(account.data.as_slice())
-        {
-            return Some(carbon_core::account::DecodedAccount {
-                lamports: account.lamports,
-                data: TokenMetadataAccount::UseAuthorityRecord(decoded_account),
-                owner: account.owner,
-                executable: account.executable,
-                rent_epoch: account.rent_epoch,
-            });
-        }
-
-        None
+        Some(carbon_core::account::DecodedAccount {
+            lamports: account.lamports,
+            data: data_enum,
+            owner: account.owner,
+            executable: account.executable,
+            rent_epoch: account.rent_epoch,
+        })
     }
+
 }
